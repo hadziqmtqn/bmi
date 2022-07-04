7e09e14115a2e20363b4746f0f7ae8dcf3f79f5c
hadziqmtqn <hadziqwebid@gmail.com>
2022-07-04 07:43:13 +0700

    add weight & height section

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -4,3 +4,5 @@ Color primaryColor = const Color(0xff0a0e21);
 TextStyle? labelTextStyle =
     TextStyle(fontSize: 18, color: Colors.white //Color(0xff8d8e98),
         );
+TextStyle numberTextStyle =
+    TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white);
